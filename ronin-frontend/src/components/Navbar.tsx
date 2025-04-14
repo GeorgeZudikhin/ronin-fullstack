9cd30088851da3467f5fbd1952aeee3eec5bfee5
GeorgeZudikhin <yehorzudikhin17@gmail.com>
2025-04-14 11:17:12 +0300

    fix: ronin logo

--- ronin-frontend/src/components/Navbar.tsx
+++ ronin-frontend/src/components/Navbar.tsx
@@ -8,11 +8,7 @@ export default function Navbar() {
                     href="#"
                     className="flex items-center space-x-3 rtl:space-x-reverse"
                 >
-                    <img
-                        src="../../public/logo.png"
-                        className="h-8"
-                        alt="Flowbite Logo"
-                    />
+                    <img src="/logo.png" className="h-8" alt="Ronin Logo" />
                     <span className="self-center text-2xl font-semibold whitespace-nowrap text-red-500">
                         Ronin Commerce
                     </span>
